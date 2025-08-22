49c7d1e54d7543e374e568231df7d4d073329360
Max Baraitser Smith <m.baraitsersmith@gmail.com>
2025-08-22 15:38:17 +0200

    hfhgffh

--- control_signal_server/control_signal_server.sh
+++ control_signal_server/control_signal_server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo ./venv/bin/python control_signal_server.py
+sudo /home/maxbaraitsersmith/Desktop/arselectronica2025-home/control_signal_server/venv/bin/python /home/maxbaraitsersmith/Desktop/arselectronica2025-home/control_signal_server/control_signal_server.py
+#sudo ./venv/bin/python control_signal_server.py
