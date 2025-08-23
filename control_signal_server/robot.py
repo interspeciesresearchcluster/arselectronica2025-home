2494fc3b928cc655988b673e3dd78659775d1bdf
Max Baraitser Smith <m.baraitsersmith@gmail.com>
2025-08-23 11:59:13 +0200

    fwafwa

--- control_signal_server/robot.py
+++ control_signal_server/robot.py
@@ -6,14 +6,19 @@ PORT = "/dev/ttyUSB0"
 BAUD = 115200
 KEYS = '1q3e4r5t0z'
 
+ardu = None
+
 def init():
+    global ardu
     print("Initialising Robot")
     ardu= serial.Serial(PORT, BAUD, timeout=.1)
     print("Initialised Robot")
 
 def move(message):
+    global ardu
    for i in range (10):
         ardu.write(message.encode())
 
 def finish():
+    global ardu
     ardu.close()
