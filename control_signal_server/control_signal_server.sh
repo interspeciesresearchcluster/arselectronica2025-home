fa14b661ee6e57b97de5e6d14f775099e4e37c84
Max Baraitser Smith <m.baraitsersmith@gmail.com>
2025-08-27 15:37:32 +0200

    wafaw

--- control_signal_server/control_signal_server.sh
+++ control_signal_server/control_signal_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo /home/maxbaraitsersmith/Desktop/arselectronica2025-home/control_signal_server/venv/bin/python /home/maxbaraitsersmith/Desktop/arselectronica2025-home/control_signal_server/control_signal_server.py -u
+sudo /home/maxbaraitsersmith/Desktop/arselectronica2025-home/control_signal_server/venv/bin/python /home/maxbaraitsersmith/Desktop/arselectronica2025-home/control_signal_server/control_signal_server.py -u  2>&1
