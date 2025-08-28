eb21be9eaed57d11bade44a344d60ba50e75807e
Max Baraitser Smith <m.baraitsersmith@gmail.com>
2025-08-28 12:59:04 +0200

    fwafwfwa

--- streaming_server/streaming_server_1.sh
+++ streaming_server/streaming_server_1.sh
@@ -7,18 +7,18 @@ ffmpeg \
     `#AUDIO INPUT` \
     -an  `# dont use audio` \
     `#LOW LATENCY FLAGS` \
-    `#-flags low_delay this creates pixelation for high resolutions` \
+    -flags low_delay `# this creates pixelation for high resolutions` \
     -fflags nobuffer `#apparently this reduces latency a bit` \
     `#VIDEO INPUT` \
     -r 30  `#input framerate` \
     -f v4l2  `#use video4linux2` \
     -s 1920x1080  `#input resolution` \
-    -i /dev/video0  `#input device` \
-    -input_format yuyv422  `#input format` \
+    -i /dev/video2  `#input device` \
+    -input_format h264  `#input format` \
     -r 30  `# output framerate` \
     `#TRANSCODING VIDEO` \
     -c:v mpeg2video  `#use mpeg2video encoder` \
-    -qscale:v 2  `#quality scale between around 2 and 30` \
+    -qscale:v 12  `#quality scale between around 2 and 30` \
     `#TRANSMITTING` \
     -f mpegts - `# output format: mpeg transport stream` \
     | nc -l -p 19062 `#send output over tcp`;
