b5c8d9dbbab739ca80b90b48d75538e76ff1d38a
Max Baraitser Smith <m.baraitsersmith@gmail.com>
2025-09-02 13:40:47 +0200

    afwafw

--- streaming_server/streaming_server_1.bash
+++ streaming_server/streaming_server_1.bash
@@ -20,20 +20,22 @@ NETCAT_PID=$!
 ffmpeg \
     `#-loglevel debug ` \
     -err_detect explode \
+    -flags low_delay `# this creates pixelation for high resolutions` \
+    -fflags nobuffer `#apparently this reduces latency a bit` \
     `#AUDIO INPUT` \
     -an  `# dont use audio` \
     `#LOW LATENCY FLAGS` \
     `#VIDEO INPUT` \
     -r 25  `#input framerate` \
     -f v4l2  `#use video4linux2` \
-    -s 1280x720  `#input resolution` \
+    -s 1920x1080  `#input resolution` \
     -i /dev/video4  `#input device` \
     -input_format h264  `#input format` \
     -r 25  `# output framerate` \
     `#TRANSCODING VIDEO` \
     `# -vf "crop=1000:1000:200:200" crop` \
-    -c:v copy  `#use mpeg2video encoder` \
-    -qscale:v 6  `#quality scale between around 2 and 30` \
+    -c:v copy  `#encoder: copy, mpeg2video / libx264 ` \
+    `#-qscale:v 6 quality scale between around 2 and 30` \
     `#TRANSMITTING` \
     -f mpegts - `# output format: mpeg transport stream` \
     > "$FIFO" &
